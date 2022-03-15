eae71c78552e70a10fb369bd103c5611554d1200
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-15 19:04:28 -0300

    ver01 finalizada

--- Program.cs
+++ Program.cs
@@ -3,9 +3,30 @@ using System.Globalization;
 
 class Program
 {
+    static double Pi = 3.14; 
     static void Main(string[] args)
     {
-        System.Console.Write("Informe um número: ");
-        double banana = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        System.Console.Write("Informe o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circ = Circunferencia(raio);
+
+        System.Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        double volume = Volume(raio);
+
+        System.Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        System.Console.WriteLine($"Valor de Pi: {Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+
+    static double Circunferencia(double r)
+    {
+        return 2 * Pi * r;
+    }
+
+    static double Volume(double r)
+    {
+        return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
     }
 }
