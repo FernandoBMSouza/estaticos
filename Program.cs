72f9d6877b12f0ce65aacc48886a797b95ea40ac
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-15 18:43:00 -0300

    iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Informe um número: ");
+        double banana = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    }
+}
